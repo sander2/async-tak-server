1b74872b6880ce6b84c9ee1221d9a459ac85b32a
Sander Bosma <sanderbosma@gmail.com>
2019-05-26 17:48:38 +0200

    cleanup

--- app.js
+++ app.js
@@ -10,100 +10,65 @@ var bodyParser = require('body-parser');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 var _ = require('lodash');
 
-var cn = {
-  host: 'localhost', // 'localhost' is the default
-  port: 5432, // 5432 is the default;
-  database: 'takdb',
-  user: 'takserver3',
-  password: 'defaultpass'
-};
+const port = 3000;
+const ptn_ninja_path = require('os').homedir() + '/workspace/tak-async/PTN-Ninja/';
 
-var db = pgp(cn);
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, done) {
-    db.one('SELECT * from login where username = $1', username)
-    .then(function (logindata) {
-      console.log('DATA:', logindata.passhash)
-      return bcrypt.compare(password, logindata.passhash);
-    })
-    .then(function (res) {
-      if (!res) {
-        throw "Invalid password";
-      }
-      console.log("Login succesful for", username);
-      return done(null, username);
-    })
-    .catch(function (error) {
-      console.log('ERROR:', error)
-      return done(error);
-    });
-}));
-app.use(require('express-session')({
-  secret: '123',
-  cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
-  },
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false } // Remember to set this
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
- 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
+// Load PTN module
 requirejs(["ptn/js/app/game", "ptn/js/app/board", "ptn/js/app/game/move"], function(Game, Board, Move) {
-  // var x = new Board();
-  // global.app = x;
-  // var game = new Game(x);
-  // x.init(game);
-  // // game.parse('[Site "PlayTak.com"]\n[Event "Online Play"]\n[Date "2018.09.19"]\n[Time "21:22:37"]\n[Player1 "Whitez"]\n[Player2 "Blackz"]\n[Clock "10:0 +20"]\n[Result "R-0"]\n[Size "6"]\n1. a1 a1\n2. d3 e5\n3. e3 d5\n4. f3 c5\n5. c3 b3\n6. c2 b2\n7. c1 b5\n8. f4 f5\n9. 1f6-1 f6\n10. Ce6 1b2>1\n11. d2 a5\n12. 2f5<2 Ce4\n13. 1e6-1 d4\n14. d1 1e4-1\n15. c4 b4\n16. c6 2e3<2\n17. f2 e4\n18. f5 e6\n19. 1c6-1 b2\n20. f1 1e4>1\n21. c6 2c2+2\n22. 1c4-1 3d3<3\n23. b6 a2\n24. 1b6-1 c4\n25. 2c5-2 d3\n26. 3c4>3 a4\n27. a6 a3\n28. 4e5<31 e4\n29. e3 1e4-1\n30. 1f5-1 2e3>2\n31. 2f4-2 1d3+1\n32. 4d5-4 d5\n33. 5d4+5 d3\n34. 4f3<13 R-0');
-  // game.parse('[Site "PlayTak.com"]\n[Event "Online Play"]\n[Date "2018.09.19"]\n[Time "21:22:37"]\n[Player1 "Whitez"]\n[Player2 "Blackz"]\n[Clock "10:0 +20"]\n[Result "R-0"]\n[Size "6"]\n1. a1 f6\n2. d3 e5\n3. e3 d5\n4. f3 c5\n5. c3 b3\n6. c2 b2\n7. c1 b5\n8. f4 f5\n9. 1f6-1 f6\n10. Ce6 1b2>1\n11. d2 a5\n12. 2f5<2 Ce4\n13. 1e6-1 d4\n14. d1 1e4-1\n15. c4 b4\n16. c6 2e3<2\n17. f2 e4\n18. f5 e6\n19. 1c6-1 b2\n20. f1 1e4>1\n21. c6 2c2+2\n22. 1c4-1 3d3<3\n23. b6 a2\n24. 1b6-1 c4\n25. 2c5-2 d3\n26. 3c4>3 a4\n27. a6 a3\n28. 4e5<31 e4\n29. e3 1e4-1\n30. 1f5-1 2e3>2\n31. 2f4-2 1d3+1\n32. 4d5-4 d5\n33. 5d4+5 d3\n34. 4f3<13 R-0');
-  // x.last(false, true);
-  console.log("Loaded ptn module");
+  var db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'takdb',
+    user: 'takserver3',
+    password: 'defaultpass'
+  });
+  
+  // setup user verification
+  passport.use(new Strategy(
+    function(username, password, done) {
+      db.one('SELECT * from login where username = $1', username)
+      .then(function (logindata) {
+        return bcrypt.compare(password, logindata.passhash);
+      })
+      .then(function (res) {
+        if (!res) {
+          throw "Invalid password";
+        }
+        console.log("Login succesful for", username);
+        return done(null, username);
+      })
+      .catch(function (error) {
+        console.log('Login error:', error)
+        return done(error);
+      });
+  }));
+  
+  // Use cookie to remember user login
+  app.use(require('express-session')({
+    secret: '123',
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    },
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // Remember to set this for non-ssl
+  }));
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
 
-
-  // var db = pgp('postgres://takserver3:defaultpass@localhost:5432/takdb');
-  const port = 3000
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
 
-  var hash = bcrypt.hashSync('mypass', 10);
-  var res = bcrypt.compareSync("mypass", hash);
-  console.log(`Result: ${res}, ${hash}`);
 
-
-  // bcrypt.hash('mypass2', 10, function(err, hash) {
-  //   db.one('INSERT INTO login (username, passhash) VALUES ($1, $2)', ['q', hash])
-  //   .then(function (gamedata) {});
-  // })
-
-  // db.one('SELECT * from login where username = $1', 'henk')
-  // .then(function (data) {
-  //   console.log('DATA:', data.passhash)
-  //   var res = bcrypt.compareSync("myp1ass", data.passhash);
-  //   console.log(`DB Result: ${res}`);
-  // })
-  // .catch(function (error) {
-  //   console.log('ERROR:', error)
-  // })
-
-  // app.get('/', (req, res) => {
-  //   // res.send('Hello World!')
-  //   res.redirect('/index.html');
-  // })
-
+  // Keep track of a list of all open websockets (which are used for notifications)
   var allSockets = [];
   app.ws('/mysocket',
    function(ws, req) {
@@ -126,31 +91,32 @@ requirejs(["ptn/js/app/game", "ptn/js/app/board", "ptn/js/app/game/move"], funct
   });
 
 
+  // handle POST request that submits a move
   app.post('/domove',
     ensureLoggedIn('/login'),
     function(req, res) {
       console.log(`Received move from ${req.user}`);
-      var q = req.body;
+      var body = req.body;
       
-      db.one('SELECT * FROM games WHERE active_player=$1 AND gameID=$2', [req.user, q.gameID])
+      db.one('SELECT * FROM games WHERE active_player=$1 AND gameID=$2', [req.user, body.gameID])
       .then(function (gamedata) {
-        var x = new Board();
-        global.app = x;
-        var game = new Game(x);
-        x.init(game);
-
+        // load PTN from the database into a game object
+        var board = new Board();
+        global.app = board;
+        var game = new Game(board);
+        board.init(game);
         game.parse(gamedata.ptn);
+        board.current_ply = game.plys[game.plys.length-1];
 
-        x.current_ply = game.plys[game.plys.length-1];
+        // insert the received move
         game.insert_ply(
-          q.move,
-          x.current_branch,
+          body.move,
+          board.current_branch,
           Math.floor(game.plys.length/2) + 1,
           game.plys.length%2+1
         );
-        x.validate(game);
+        board.validate(game);
 
-        console.log(game.print_text());
         if (!game.is_valid) {
           console.log(game.print_text());
           throw 'Invalid move attempt';
@@ -160,91 +126,60 @@ requirejs(["ptn/js/app/game", "ptn/js/app/board", "ptn/js/app/game/move"], funct
         var thisplayer  = gamedata.player1 == req.user ? gamedata.player1 : gamedata.player2;
         var opponent    = gamedata.player1 != req.user ? gamedata.player1 : gamedata.player2;
 
-        var ret = db.none('UPDATE games set ptn=$1, active_player=$2 where gameID=$3', [game.print_text(), next_player, q.gameID]);
+        // update the ptn in the database
+        var ret = db.none('UPDATE games set ptn=$1, active_player=$2 where gameID=$3', [game.print_text(), next_player, body.gameID]);
 
+        // send notification to the opponent
         var other = allSockets.find(x => x.user == opponent);
         if (!_.isUndefined(other)) {
-          other.send(JSON.stringify({opponent:thisplayer, gameID:q.gameID}));
+          other.send(JSON.stringify({opponent:thisplayer, gameID:body.gameID}));
         }
         return ret;
       })
       .catch(function (error) {
-        console.log('ERROR for ' + q.username + ': ', error);
+        console.log('ERROR for ' + body.username + ': ', error);
       });
     }
   );
 
-
+  // handle get request used to get the ptn with a given gameID
   app.get('/getgame',
   ensureLoggedIn('/login') ,
   function(req, res) {
-    console.log(`getgame for ${req.user} with id ${req.query.id}`);
     db.one('SELECT * FROM games WHERE (player1=$1 OR player2=$1) AND gameID=$2', [req.user, req.query.id])
     .then(function (gamedata) {
       res.send(JSON.stringify({ptn: gamedata.ptn}));
     });
   });
 
-  app.get('/error', (req, res) => {
-    res.send('Error; not logged in!')
-  })
-app.get('/login',
+
+  // quick login form
+  app.get('/login',
   function(req, res){
-    res.send('<form action="/login23" method="post">\n<div>\n<label>Username:</label>\n<input type="text" name="username" id="username"/><br/>\n</div>\n<div>\n<label>Password:</label>\n<input type="password" name="password" id="password"/>\n</div>\n<div>\n<input type="submit" value="Submit"/>\n</div>\n</form>');
+    res.send('<form action="/dologin" method="post">\n<div>\n<label>Username:</label>\n<input type="text" name="username" id="username"/><br/>\n</div>\n<div>\n<label>Password:</label>\n<input type="password" name="password" id="password"/>\n</div>\n<div>\n<input type="submit" value="Submit"/>\n</div>\n</form>');
   });
-  app.get('/failed',
-  function(req, res){
-    res.send('isAuthenticated: ' + req.isAuthenticated());
-  });
-app.post('/login23', 
-passport.authenticate('local', { failureRedirect: '/error' }),
+
+  // performing a login action
+  app.post('/dologin', 
+  passport.authenticate('local', { failureRedirect: '/error' }),
   function(req, res) {
-    var adasd = req.body;
-    res.redirect('/games');
+    res.redirect('/games'); // executed iff login was succesful
   });
 
-  // app.post('/login23', function(req, res, next) {
-  //   passport.authenticate('local', function(err, user, info) {
-  //     if (err) { return next(err); }
-  //     if (!user) { return res.redirect('/login'); }
-  //     req.logIn(user, function(err) {
-  //       if (err) { return next(err); }
-  //       return res.redirect('/users/' + user.username);
-  //     });
-  //   })(req, res, next);
-  // });
+  // login failure page
+  app.get('/error', (req, res) => {
+    res.send('Error; failed to log in!')
+  })
 
-// app.post('/login23', 
-//   function(request, response, next) {
-//     console.log(request.session)
-//     passport.authenticate('local', 
-//     function(err, user, info) {
-//       if(!user){ response.send(info.message);}
-//       else{
-//         request.login(user, function(error) {
-//             if (error) return next(error);
-//             console.log("Request Login supossedly successful.");
-//             return response.send('Login successful');
-//         });
-//         //response.send('Login successful');
-//       }
-
-//     })(request, response, next);
-//   }
-// );
-app.get('/logout',
+  // allow user to log out  
+  app.get('/logout',
   function(req, res){
     req.logout();
-    res.redirect('/bka');
+    res.redirect('/login');
   });
 
-app.get('/profile',
-  ensureLoggedIn('/failed') ,
-  function(req, res){
-    res.send("Great success!");
-    // res.render('profile', { user: req.user });
-});
-app.get('/games',
+  // Get the game overview page
+  app.get('/games',
   ensureLoggedIn('/login') ,
   function(req, res){
     var overall_template = _.template(
@@ -263,31 +198,31 @@ app.get('/games',
       '</html>'
     );
     var item_template = _.template('<a href="/index.html?gameid=<%= linkid %>">Versus <%= name %></a>');
+
+    // get a list of the user's games and return them
     db.any('SELECT * FROM games WHERE player1=$1 OR player2=$1', req.user)
     .then(function (gamedata) {
-      var tq = _.groupBy(gamedata, x => x.active_player == req.user);
+      // group the games by whether it's player's turn or their opponent's 
+      var grouped = _.groupBy(gamedata, x => x.active_player == req.user);
       var fn = y => _
         .chain(y)
         .map(x => item_template({linkid: x.gameid, name: x.player1 == req.user ? x.player2 : x.player1}))
         .join('\n\n')
         .value();
       
-      
-      var result = overall_template({yourturn: fn(tq[true]), theirturn: fn(tq[false])});
+      var result = overall_template({yourturn: fn(grouped[true]), theirturn: fn(grouped[false])});
       res.send(result);
     });
+  });
 
-  // res.render('profile', { user: req.user });
-});
-app.get(/^(.+)$/, 
+  // Serve all files used by ptn-ninja
+  app.get(/^(.+)$/, 
   ensureLoggedIn('/login') ,
   function(req, res)
-{ 
-  // console.log('static file request : ' + req.params);
-  var f = '/home/sander/workspace/tak-async/PTN-Ninja/' + req.params[0];
-  // console.log('Serving' + f);
-  res.sendfile(f); 
-});
+  { 
+    var f = ptn_ninja_path + req.params[0];
+    res.sendfile(f); 
+  });
 
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  app.listen(port, () => console.log(`App listening on port ${port}!`));
 });
